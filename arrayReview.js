ac483b6efee65646f34d9fc623a6e8bbd9ac7bcc
whitneypage <wpagegarner@gmail.com>
2015-04-08 19:47:21 -0600

    Worked on these guys

--- arrayReview.js
+++ arrayReview.js
@@ -2,7 +2,12 @@ var threeItems = [1,2,3];
 //create a function named 'last' that returns the last item from 'threeItems'
 //alert the result of your function
 
-  //code here
+  var last = function(arr) {
+       return arr[arr.length - 1];
+  }
+
+  last(threeItems);
+
 
 
 
@@ -13,7 +18,18 @@ var threeItems = [1,2,3];
 //Loop through evenArray removing all values that aren't even 
 var evenArray = [1,2,3,6,22,98,45,23,22,12];
 
-  //code here
+var evens = function(arr) {
+   for (var i = 0; i < arr.length; i++) {
+   	  if (arr[i] % 2 !== 0) {
+   		  arr.splice(i, 1);
+   		  i--;
+   	    }
+    }
+
+    return arr;
+}
+
+evens(evenArray);
 
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
@@ -26,8 +42,19 @@ var getRandomArbitrary = function() {
 }
 var randomArray = [0,3,4,5,6,7,9,14,17,24,25,26,29,30];
 
-  //code here
+  var match = function(arr) {
+  	var num = getRandomArbitrary();
+  	for (var i = 0; i < arr.length; i++) {
+  		if (num === arr[i]) {
+  		  console.log(num);
+  		  return true;
+  		} 
+  	}
+  	console.log(num);
+  	return false;
+ }
 
+match(randomArray);
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
@@ -37,21 +64,37 @@ var randomArray = [0,3,4,5,6,7,9,14,17,24,25,26,29,30];
 var first = [1,2,3,4,5];
 var second;
 
-  //code here
-
+second = first.slice();
+second.push(6, 7);
 alert(first) //[1,2,3,4,5];
 alert(second) //[1,2,3,4,5,6,7];
-
 
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
 
 
 
-//write a function called longest that takes in our sentence variable, and returns the longest word in that sentence.
-var sentence = "Dev Mountain is the best"
 
-  //code here
+
+//write a function called longest that takes in our sentence variable, and returns the longest word in that sentence.
+var sentence = "Dev Mountain is the best";
+var long = 0;
+var theWord;
+
+var longest = function(sentence) {
+	var strArr = sentence.split(" ");
+	for(var i = 0; i < strArr.length; i++) {
+		if(strArr[i].length > long) {
+		    long = strArr[i].length;
+			theWord = strArr[i];
+		}
+	}
+
+	return theWord;
+}
+
+longest(sentence); 
+
 
 
 /* NEXT PROBLEM - NEXT PROBLEM - NEXT PROBLEM */
@@ -61,8 +104,19 @@ var sentence = "Dev Mountain is the best"
 //write a function called capitalize that takes in the myPoem variable and capitalizes every word 
 var myPoem = 'What is a jQuery but a misunderstood object?'
 //What is a jQuery but a misunderstood object? --> What Is A JQuery But A Misunderstood Object?
+var capitalize = function(string) {
+  var words = string.split(" ");
+  for(var i = 0; i < words.length; i++) {
+  	var letters = words[i].split(" ");
+  	console.log(letters);
+  	words[i].charAt(0).toUpperCase();
+   }
+  return words.join(" ");
+}
 
-  //code here
+capitalize(myPoem);
+
+
 
 
 
@@ -72,3 +126,34 @@ var myPoem = 'What is a jQuery but a misunderstood object?'
 
 var theOdyssey = "function expression or function declaration? Tis an obvious choice";
 //Write a function called vowelCounter that takes in a string (theOdyssey) and returns how many vowels are in that string.
+
+var vowelCounter = function(str) {
+	var count = 0
+	for (var i = 0; i < str.length; i++) {
+		 switch (str.charAt(i)) {
+		 	case "a":
+		 	case "e":
+		 	case "i":
+		 	case "o":
+		 	case "u":
+		 	  count++;
+		 	  break;
+		 	default:
+		 	  break;  
+		 
+	}   
+   return count;
+  }
+};
+
+vowelCounter(theOdyssey);
+
+ 
+
+
+
+
+
+
+
+
--- objectReview.js
+++ objectReview.js
@@ -1,18 +1,27 @@
 //Make a 'favoriteThings' object that contains the following keys: band, food, person, book, movie, holiday. Have the values to those keys be your favorite thing in that category. 
+var favoriteThings = {
+  band: 'Spice Girls',
+  food: 'carrots',
+  person: 'Page',
+  book: 'The Road',
+  movie: 'The Road',
+  holiday: 'easter'
+}
 
-  //Your code here
 
 //After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
 
-  //Your code here
+favoriteThings.car = 'Toyota';
 
 //Now change the food key in your favoriteThings object to be 'Lettuce' and change the book key in your favoriteThings object to be '50 Shades of Gray'. 
 
-  //Your code here
+favoriteThings.food = 'Lettuce';
+favoriteThings.book = '50 Shades';
 
 //Now, alert your favorite person, then alert your favorite book.
 
-  //Your code here
+alert(favoriteThings.person);
+alert(favoriteThings.book);
 
 
 
@@ -31,8 +40,6 @@ var user = {
 
 //Above you're given a user object. Loop through the user object checking to make sure that each value is truthy. If it's not truthy, remove it from the object. hint: 'delete'.
 
-  //Your code here
-
 //Once you get your truthy object, Change the remaining values in the object to be specific to you (name: 'your name', username: 'your username'), rather than my information.
 
   //Your code here
@@ -48,16 +55,21 @@ var user = {
 
 
 //create an empty object called methodCollection. 
-
-  //Your code here
+var methodCollection = {};
 
 //Now add two methods (functions that are properties on objects) to your methodCollection object. One called 'alertHello' which alerts 'hello' and another method called logHello which logs 'hello' to the console. 
 
-  //Your code here
+methodCollection.alertHello = function() {
+    alert("hello");
+}
 
+methodCollection.logHello = function() {
+    console.log("hello");
+}
 //Now call your alertHello and logHello methods. 
 
-  //Your code here
+ methodCollection.alertHello();
+ methodCollection.logHello();
 
 
 
